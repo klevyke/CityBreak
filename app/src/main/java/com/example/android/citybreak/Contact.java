31a5575643e73d845c36bcd8232c38116b34d230
Kopacz Levente <office@imageservices.ro>
2018-04-12 08:42:54 +0300

    Contact class created

---
+++ app/src/main/java/com/example/android/citybreak/Contact.java
@@ -0,0 +1,69 @@
+package com.example.android.citybreak;
+
+/**
+ * Created by Levy on 12.04.2018.
+ */
+
+public class Contact {
+    private String contactAddress;
+    private String contactPhone;
+    private String contactWeb;
+    private Boolean contactPhoneSet = true;
+    private Boolean contactWebSet = true;
+
+    /**
+     * Constructor for whole contact
+     * @param address place address
+     * @param phone place phone number
+     * @param web place web address
+     */
+    public Contact (String address, String phone, String web) {
+        contactAddress = address;
+        contactPhone = phone;
+        contactWeb = web;
+    }
+
+    /**
+     * Constructor without web address
+     * @param address place address
+     * @param phone place phone number
+     */
+    public Contact (String address, String phone) {
+        contactAddress = address;
+        contactPhone = phone;
+        contactWebSet = false;
+    }
+
+    /**
+     * Constructor without web address and phone
+     * @param address place address
+     */
+    public Contact (String address) {
+        contactAddress = address;
+        contactWebSet = false;
+        contactPhoneSet = false;
+    }
+
+    // Get the address
+    public String getPlaceAddress() {
+        return contactAddress;
+    }
+    // Get the phone number
+    public String getPlaceTelephone() {
+        return contactPhone;
+    }
+    // Get the web address
+    public String getPlaceWeb() {
+        return contactWeb;
+    }
+
+    // Check if phone is set
+    public Boolean hasPhoneSet() {
+        return contactPhoneSet;
+    }
+
+    // Check if phone is set
+    public Boolean hasWebSet() {
+        return contactWebSet;
+    }
+}
---
+++ app/src/main/java/com/example/android/citybreak/Restaurants.java
@@ -0,0 +1,9 @@
+package com.example.android.citybreak;
+
+/**
+ * Created by Levy on 12.04.2018.
+ */
+
+public class Restaurants extends Places {
+
+}
