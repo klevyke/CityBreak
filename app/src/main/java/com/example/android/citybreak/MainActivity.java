5c80cfb8e9e9e91dd5c939cc731069e1a08485fe
Kopacz Levente <office@imageservices.ro>
2018-04-12 11:32:08 +0300

    Main layout and activity created

--- app/src/main/java/com/example/android/citybreak/MainActivity.java
+++ app/src/main/java/com/example/android/citybreak/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.citybreak;
 
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
